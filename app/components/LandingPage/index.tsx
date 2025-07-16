1e53c3c9b46ec289194d5b677b664d25f130d618
TrAyesha123 <qaziayesha966@gmail.com>
2025-07-17 00:29:24 +0500

    adding

--- app/components/LandingPage/index.tsx
+++ app/components/LandingPage/index.tsx
@@ -2,10 +2,12 @@
 import Faq from "./faq";
 import Contactus from "./contactus"
 import Hero from "./hero";
+import ProjectSlider from "./projectSlider"
 export default function LandingPage() {
   return (
     <div className="bg-[#FAFAFA]">
       <Hero />
+      <ProjectSlider/>
       <Contactus />
       <Faq />
     </div>
---
+++ app/components/LandingPage/projectSlider.tsx
@@ -0,0 +1,217 @@
+import Image from "next/image";
+
+export default function ProjectSlider() {
+  return (
+    <div className="pt-[26.5px] pb-[10.98] flex flex-col gap-[24px]">
+      <div className="w-full overflow-hidden">
+        <div className="w-max flex animate-slide gap-[0px]">
+          {/* First set of images */}
+          <Image
+            src="/slider/lineAp1.png"
+            alt="p1"
+            height={315.9}
+            width={492}
+            className="max-[900px]:h-[280px] max-[900px]:w-[430px] max-[600px]:h-[220px] max-[600px]:w-[380px]"
+          />
+          <Image
+            src="/slider/lineAp2.png"
+            alt="p2"
+            height={315.9}
+            width={145.8}
+            className="max-[900px]:h-[280px] max-[600px]:h-[220px] max-[600px]:w-[120px]"
+          />
+          <Image
+            src="/slider/lineAp3.png"
+            alt="p3"
+            height={315.9}
+            width={492.4}
+            className="max-[900px]:h-[280px] max-[900px]:w-[430px] max-[600px]:h-[220px] max-[600px]:w-[380px]"
+          />
+          <Image
+            src="/slider/lineAp4.png"
+            alt="p4"
+            height={315.9}
+            width={492.4}
+            className="max-[900px]:h-[280px] max-[900px]:w-[430px] max-[600px]:h-[220px] max-[600px]:w-[380px]"
+          />
+          <Image
+            src="/slider/lineAp5.png"
+            alt="p5"
+            height={315.9}
+            width={145.8}
+            className="max-[900px]:h-[280px] max-[600px]:h-[220px] max-[600px]:w-[120px] "
+          />
+
+          {/* Duplicate the same set to allow seamless scroll */}
+          <Image
+            src="/slider/lineAp1.png"
+            alt="p1"
+            height={315.9}
+            width={492}
+            className="max-[900px]:h-[280px] max-[900px]:w-[430px] max-[600px]:h-[220px] max-[600px]:w-[380px]"
+          />
+          <Image
+            src="/slider/lineAp2.png"
+            alt="p2"
+            height={315.9}
+            width={145.8}
+            className="max-[900px]:h-[280px] max-[600px]:h-[220px] max-[600px]:w-[120px]"
+          />
+          <Image
+            src="/slider/lineAp3.png"
+            alt="p3"
+            height={315.9}
+            width={492.4}
+            className="max-[900px]:h-[280px] max-[900px]:w-[430px] max-[600px]:h-[220px] max-[600px]:w-[380px]"
+          />
+          <Image
+            src="/slider/lineAp4.png"
+            alt="p4"
+            height={315.9}
+            width={492.4}
+            className="max-[900px]:h-[280px] max-[900px]:w-[430px] max-[600px]:h-[220px] max-[600px]:w-[380px]"
+          />
+          <Image
+            src="/slider/lineAp5.png"
+            alt="p5"
+            height={315.9}
+            width={145.8}
+            className="max-[900px]:h-[280px] max-[600px]:h-[220px]  max-[600px]:w-[120px]"
+          />
+        </div>
+      </div>
+      <div className="w-full overflow-hidden">
+        <div className="w-max flex animatedd-slide gap-[0px]">
+          {/* First set of images */}
+          <Image
+            src="/slider/l2Ap1.png"
+            alt="p1"
+            height={315.9}
+            width={492}
+            className="max-[900px]:h-[280px] max-[900px]:w-[430px] max-[600px]:h-[220px] max-[600px]:w-[380px]"
+          />
+          <Image
+            src="/slider/l2Ap2.png"
+            alt="p2"
+            height={315.9}
+            width={145.8}
+            className="max-[900px]:h-[280px] max-[600px]:h-[220px] max-[600px]:w-[120px] "
+          />
+          <Image
+            src="/slider/l2Ap3.png"
+            alt="p3"
+            height={315.9}
+            width={540.5}
+            className="max-[900px]:h-[280px] max-[900px]:w-[470px] max-[600px]:h-[220px] max-[600px]:w-[400px]"
+          />
+          <Image
+            src="/slider/l2Ap4.png"
+            alt="p4"
+            height={315.9}
+            width={145.8}
+            className="max-[900px]:h-[280px] max-[600px]:h-[220px] max-[600px]:w-[120px]"
+          />
+          <Image
+            src="/slider/l2Ap5.png"
+            alt="p5"
+            height={315.9}
+            width={145.8}
+            className="max-[900px]:h-[280px] max-[600px]:h-[220px] max-[600px]:w-[120px]"
+          />
+          <Image
+            src="/slider/l2Ap6.png"
+            alt="p5"
+            height={315.9}
+            width={145.8}
+            className="max-[900px]:h-[280px] max-[600px]:h-[220px] max-[600px]:w-[120px]"
+          />
+          <Image
+            src="/slider/l2Ap7.png"
+            alt="p5"
+            height={315.9}
+            width={492.4}
+            className="max-[900px]:h-[280px] max-[900px]:w-[430px] max-[600px]:h-[220px] max-[600px]:w-[380px]"
+          />
+          <Image
+            src="/slider/l2Ap8.png"
+            alt="p5"
+            height={315.9}
+            width={145.8}
+            className="max-[900px]:h-[280px] max-[600px]:h-[220px] max-[600px]:w-[120px]"
+          />
+          <Image
+            src="/slider/l2Ap9.png"
+            alt="p5"
+            height={315.9}
+            width={145.8}
+            className="max-[900px]:h-[280px] max-[600px]:h-[220px] max-[600px]:w-[120px] "
+          />
+
+          <Image
+            src="/slider/l2Ap1.png"
+            alt="p1"
+            height={315.9}
+            width={492}
+            className="max-[900px]:h-[280px] max-[900px]:w-[430px] max-[600px]:h-[220px] max-[600px]:w-[380px]"
+          />
+          <Image
+            src="/slider/l2Ap2.png"
+            alt="p2"
+            height={315.9}
+            width={145.8}
+            className="max-[900px]:h-[280px] max-[600px]:h-[220px] max-[600px]:w-[120px] "
+          />
+          <Image
+            src="/slider/l2Ap3.png"
+            alt="p3"
+            height={315.9}
+            width={540.5}
+            className="max-[900px]:h-[280px] max-[900px]:w-[470px] max-[600px]:h-[220px] max-[600px]:w-[400px]"
+          />
+          <Image
+            src="/slider/l2Ap4.png"
+            alt="p4"
+            height={315.9}
+            width={145.8}
+            className="max-[900px]:h-[280px] max-[600px]:h-[220px] max-[600px]:w-[120px] "
+          />
+          <Image
+            src="/slider/l2Ap5.png"
+            alt="p5"
+            height={315.9}
+            width={145.8}
+            className="max-[900px]:h-[280px] max-[600px]:h-[220px] max-[600px]:w-[120px]"
+          />
+          <Image
+            src="/slider/l2Ap6.png"
+            alt="p5"
+            height={315.9}
+            width={145.8}
+            className="max-[900px]:h-[280px] max-[600px]:h-[220px] max-[600px]:w-[120px]"
+          />
+          <Image
+            src="/slider/l2Ap7.png"
+            alt="p5"
+            height={315.9}
+            width={492.4}
+            className="max-[900px]:h-[280px] max-[900px]:w-[430px] max-[600px]:h-[220px] max-[600px]:w-[380px]"
+          />
+          <Image
+            src="/slider/l2Ap8.png"
+            alt="p5"
+            height={315.9}
+            width={145.8}
+            className="max-[900px]:h-[280px] max-[600px]:h-[220px] max-[600px]:w-[120px]"
+          />
+          <Image
+            src="/slider/l2Ap9.png"
+            alt="p5"
+            height={315.9}
+            width={145.8}
+            className="max-[900px]:h-[280px] max-[600px]:h-[220px] max-[600px]:w-[120px]"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
