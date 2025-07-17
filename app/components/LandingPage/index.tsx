9493acf45609dbe57f547c0b595a41797305f8e9
TrAyesha123 <qaziayesha966@gmail.com>
2025-07-17 16:07:47 +0500

    adding

--- app/components/LandingPage/index.tsx
+++ app/components/LandingPage/index.tsx
@@ -1,19 +1,23 @@
-
 import Faq from "./faq";
-import Contactus from "./contactus"
+import Contactus from "./contactus";
 import Hero from "./hero";
 import ProjectSlider from "./projectSlider";
 import ManagementValue from "./managementValues";
 import StaticTestimonial from "./staticTestimonial";
 import Blogs from "./blogs";
+import Process from "./process";
+import Project from "./project";
+
 export default function LandingPage() {
   return (
-    <div className="bg-[#FAFAFA]">
+    <div className="bg-[#FAFAFA] flex flex-col gap-[8px]">
       <Hero />
-      <ProjectSlider/>
+      <ProjectSlider />
+      <Project />
+      <Process />
       <Blogs />
+      <StaticTestimonial />
       <ManagementValue />
-      <StaticTestimonial/>
       <Contactus />
       <Faq />
     </div>
